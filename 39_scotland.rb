44830b88da04c3b1f0b2dca07a74f558aea069a7
RLOG89 <ross.loggie@gmail.com>
2016-09-22 11:49:17 +0100

    tidied up and skipped unnecessary last variable

---
+++ 39_scotland.rb
@@ -0,0 +1,34 @@
+# city to abbreviation
+city = {
+  'Aberdeen' => 'ABZ',
+  'Dundee' => 'DUN',
+  'Edinburgh' => 'EDI',
+  'Glasgow' => 'GLA',
+  'Inverness' => "INV"
+}
+
+# restaurants in each city
+restaurant = {
+  'ABZ' => 'Moonfish',
+  'DUN' => 'Travelodge',
+  'EDI' => 'Martin Wishart',
+  'GLA' => 'La Lanterna',
+  'INV' => 'Premier Inn'
+}
+
+# puts the restaurants in the cities
+puts "Aberdeen city's best restaurant is #{restaurant['ABZ']}"
+puts "The best place to eat in Edinburgh is #{restaurant['EDI']}"
+puts "La Lanterna is in #{city['Glasgow']}"
+
+# mess around
+city.each do |cities, abbrev|  
+  food = restaurant[abbrev]
+  puts "#{cities} is known as #{abbrev} and its best restaurant is #{food}"
+end
+
+# use the reject function to reject restaurants beginning with letter "m"
+puts restaurant.reject {|a,b|  b =~ /M.*/}
+
+
+
